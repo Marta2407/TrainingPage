4c2ac7a338343b082274c5a7b8372d8d4e5c9383
marta galach <martagalach24@gmail.com>
2023-06-10 16:40:54 +0200

    vite bundler, directories structure added

---
+++ src/js/constants/exampleObject.js
@@ -0,0 +1,17 @@
+export const exampleObject1 = {
+  name: 'object1',
+  fields: {
+    field1: 'test11',
+    field2: 'test12',
+    alertMsg: 'TEST ALERT 1',
+  },
+};
+
+export const exampleObject2 = {
+  name: 'object2',
+  fields: {
+    field1: 'test21',
+    field2: 'test22',
+    alertMsg: 'TEST ALERT 2',
+  },
+};
