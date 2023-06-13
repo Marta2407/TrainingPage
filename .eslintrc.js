5556fc52ae1f7fab0a97f2708710b14617e8c1eb
marta galach <martagalach24@gmail.com>
2023-06-13 16:38:20 +0200

    Upgrade prettier plugins for html

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,15 @@ module.exports = {
     es2021: true,
   },
   extends: ['airbnb-base', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'eslint-plugin-html', 'html'],
+  settings: {
+    'html/html-extensions': ['.html'],
+  },
   parserOptions: {
+    parser: '@babel/eslint-parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
+    requireConfigFile: false,
   },
   rules: {
     'prefer-arrow-callback': 'warn',
